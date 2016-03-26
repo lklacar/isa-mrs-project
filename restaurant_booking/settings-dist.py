c92d7af15317f0673561e5d4ef0e0d27154e1f5f
Kristina <pinksugarpill@gmail.com>
2016-03-26 19:31:46 +0100

    added admin(manager of system) app

--- restaurant_booking/settings-dist.py
+++ restaurant_booking/settings-dist.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrapform',
+    'admin',
     'app',
     'authentication',
     'social.apps.django_app.default',
