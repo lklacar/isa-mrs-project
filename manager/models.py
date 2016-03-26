603689fddc5a08f5b0a40c7e36556f6653bee12a
Miljana Vinokic <vinokicmiljana@gmail.com>
2016-03-26 20:34:38 +0100

    Added restaurant models

--- manager/models.py
+++ manager/models.py
@@ -1,5 +1,8 @@
 from __future__ import unicode_literals
 
-from django.db import models
-
 # Create your models here.
+from authentication.models import User
+
+
+class Manager(User):
+    pass
--- restaurant/models.py
+++ restaurant/models.py
@@ -1,5 +1,26 @@
 from __future__ import unicode_literals
-
 from django.db import models
 
 # Create your models here.
+from authentication.models import User
+from manager.models import Manager
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    manager = models.OneToOneField(Manager)
+
+
+class Food(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    restaurant = models.ForeignKey(Restaurant)
+
+
+class Drink(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    restaurant = models.ForeignKey(Restaurant)
